f4de4be6484a3dc2b91f7173ef32d0cc22324ef4
Komal Sukhani <komaldsukhani@gmail.com>
2025-12-15 16:25:32 +0530

    Add graceful server shutdown

--- main.go
+++ main.go
@@ -1,18 +1,23 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/komaldsukhani/reverseproxyexample/internal"
 )
 
 const (
-	UpstreamURL = "http://example.com"
-	Port        = 8080
+	UpstreamURL     = "http://example.com"
+	Port            = 8080
+	ShutdownTimeout = 10 * time.Second
 
 	CacheTTL           = 30 * time.Second
 	MaxCacheSize       = 1 * 1024 * 1024
@@ -27,6 +32,36 @@ func main() {
 		Cache:     internal.NewMemoryCache(CacheTTL, MaxCacheSize, MaxCacheRecordSize),
 	}
 
-	slog.Info("Started server", "port", Port)
-	log.Fatal(http.ListenAndServe(addr, &p))
+	srv := http.Server{
+		Addr: addr,
+	}
+
+	go func() {
+		slog.Info("Started server", "port", Port)
+
+		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+			log.Fatalf("failed to start server: %v", err)
+		}
+
+		log.Fatal(http.ListenAndServe(addr, &p))
+	}()
+
+	gracefulShutdown(&srv)
+}
+
+func gracefulShutdown(srv *http.Server) {
+	var quit = make(chan os.Signal, 1)
+
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	<-quit
+
+	slog.Info("Shutting down the server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), ShutdownTimeout)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("failed to shutdown the server: %v", err)
+	}
 }
